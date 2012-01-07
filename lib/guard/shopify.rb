9c1299c733cc12b1fdfe35df497c696c38c64c53
1337807 <jonan.scheffler@g5platform.com>
2012-01-07 12:49:08 -0800

    Omnium rerum principia parva sunt.

---
+++ lib/guard/shopify.rb
@@ -0,0 +1,11 @@
+require 'guard'
+require 'guard/guard'
+require 'shopify_api'
+
+module Guard
+  class Shopify < Guard
+    def run_on_change
+      puts 'Hot sex pooty'
+    end
+  end
+end
---
+++ lib/guard/shopify/version.rb
@@ -0,0 +1,5 @@
+module Guard
+  module Shopify
+    VERSION = "0.0.1"
+  end
+end
